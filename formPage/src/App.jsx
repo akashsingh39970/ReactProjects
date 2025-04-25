9cea601e816f21aed8fe443f14317b347be64aa9
akashsingh39970 <akashsingh39970@gmail.com>
2025-04-25 17:52:26 +0530

    html update

--- formPage/src/App.jsx
+++ formPage/src/App.jsx
@@ -1,34 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [firstName, setfirstName] = useState('');
+  const [lastName, setlastName] = useState('');
+  const [emailId, setemailId] = useState('');
+  const [contactNumber, setcontactNumber] = useState('');
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='formmain'>
+      <form action="">
+        <fieldset>
+          {/* <legend>form Page</legend> */}
+          <h2>Form Submissiom Page</h2>
+          <div className='formfield'>
+            <label htmlFor="first_name">First Name</label>
+            <input type="text" name='first_name' id='first_name' value={firstName} placeholder='Enter First Name' />
+          </div>
+
+
+          <div className='formfield'>
+            <label htmlFor="last_name">Last Name</label>
+            <input type="text" name='last_name' id='last_name' value={lastName} placeholder='Enter Last Name' />
+          </div>
+
+
+          <div className='formfield'>
+            <label htmlFor="email_id">Email Address</label>
+            <input type="email" name='email_id' id='email_id' value={emailId} placeholder='Enter Email id' />
+          </div>
+
+
+          <div className='formfield'>
+            <label htmlFor="contact_number">Contact Number</label>
+            <input type="tel" name='contat_number' id='contat_number' value={contactNumber} placeholder='Enter Contact Number' />
+          </div>
+
+
+          <div className="formfield">
+            <label htmlFor="gender">Gender</label>
+            <div className="radio-group">
+              <input type="radio" id="male" name="gender" value="male" />
+              <label htmlFor="male">Male</label>
+
+              <input type="radio" id="female" name="gender" value="female" />
+              <label htmlFor="female">Female</label>
+
+              <input type="radio" id="others" name="gender" value="others" />
+              <label htmlFor="others">Others</label>
+            </div>
+          </div>
+
+          <div className="formfield">
+            <label htmlFor="skill">Select Your Skill</label>
+            <div className="radio-group">
+              <input type="checkbox" id="react" name="react" value="react" />
+              <label htmlFor="react">React</label>
+
+              <input type="checkbox" id="nodejs" name="nodejs" value="nodejs" />
+              <label htmlFor="nodejs">Node js</label>
+
+              <input type="checkbox" id="reactjs" name="reactjs" value="reactjs" />
+              <label htmlFor="reactjs">Next js</label>
+            </div>
+          </div>
+
+          <div className='formfield'>
+            <label htmlFor="">Upload Resume</label>
+            <input type="upload" />
+          </div>
+
+
+
+
+
+
+        </fieldset>
+      </form>
+
+    </div>
   )
 }
 
