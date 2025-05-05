2ea06721f18e2f53bb7926c516c738b5b53b822f
akashsingh39970 <akashsingh39970@gmail.com>
2025-05-05 17:53:54 +0530

    updating

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,10 +1,20 @@
 
 import Home from './assets/Pages/home/Home';
+import Login from './assets/Pages/login/Login';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className='app'>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/'>
+            <Route index element={<Home/>}/>
+            <Route path='/login' element={<Login/>}/>
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
